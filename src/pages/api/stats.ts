51a575941e5a7939683fb021ca4b7987188934fe
Thales <81639334+cinojosa0705@users.noreply.github.com>
2023-04-26 10:59:12 -0400

    server error handling

--- src/pages/api/stats.ts
+++ src/pages/api/stats.ts
@@ -5,48 +5,68 @@ import { open } from "sqlite";
 const API_KEY = "yoyo-nono-yolo-nolo-ahhh-7777";
 
 interface dataBlock {
-    apiKey: string, userId: string, userName: string, serverId: string, serverName: string, amount: string, side: string
+  apiKey: string;
+  userId: string;
+  userName: string;
+  serverId: string;
+  serverName: string;
+  amount: string;
+  side: string;
 }
 
 async function saveData(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== "POST") {
-    res?.status(405).json({ message: "Method not allowed" });
-    return;
+  try {
+    if (req.method !== "POST") {
+      res?.status(405).json({ message: "Method not allowed" });
+      return;
+    }
+
+    const data: dataBlock = req.body as dataBlock;
+
+    if (data.apiKey !== API_KEY) {
+      res.status(403).json({ message: "Invalid API key" });
+      return;
+    }
+
+    const db = await open({
+      filename: "stats.db",
+      driver: sqlite3.Database,
+    });
+
+    await db.exec(`
+            CREATE TABLE IF NOT EXISTS stats (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id TEXT,
+            user_name TEXT,
+            server_id TEXT,
+            server_name TEXT,
+            amount REAL,
+            side TEXT
+            )
+        `);
+
+    const result = await db.run(
+      `
+            INSERT INTO stats (user_id, user_name, server_id, server_name, amount, side)
+            VALUES (?, ?, ?, ?, ?, ?)
+        `,
+      [
+        data.userId,
+        data.userName,
+        data.serverId,
+        data.serverName,
+        data.amount,
+        data.side,
+      ]
+    );
+
+    res.status(200).json({ message: "Data saved successfully", result });
+  } catch (error) {
+    console.error("Error in saveData API: ", error);
+    res
+      .status(500)
+      .json({ message: "Internal Server Error", error: error.message });
   }
-
-const data: dataBlock = req.body as dataBlock;
-
-  if (data.apiKey !== API_KEY) {
-    res.status(403).json({ message: "Invalid API key" });
-    return;
-  }
-
-  const db = await open({
-    filename: "stats.db",
-    driver: sqlite3.Database,
-  });
-
-  await db.exec(`
-    CREATE TABLE IF NOT EXISTS stats (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      user_id TEXT,
-      user_name TEXT,
-      server_id TEXT,
-      server_name TEXT,
-      amount REAL,
-      side TEXT
-    )
-  `);
-
-  const result = await db.run(
-    `
-    INSERT INTO stats (user_id, user_name, server_id, server_name, amount, side)
-    VALUES (?, ?, ?, ?, ?, ?)
-  `,
-    [data.userId, data.userName, data.serverId, data.serverName, data.amount, data.side]
-  );
-
-  res.status(200).json({ message: "Data saved successfully", result });
 }
 
 export default saveData;
